fc3836f091105819c90f62be8a1f0f8b7797e1f9
ThaoCV <che.van.thao@jam-tech.jp>
2022-11-11 16:57:09 +0900

    add button

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -18,23 +18,39 @@ export const AppointmentForm = ({
     const [month, day, year] = new Date()
       .toLocaleDateString("en-US")
       .split("/");
-    return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
+    return `${year}/${month.padStart(2, "0")}/${day.padStart(2, "0")}`;
   };
+
+  const handleTitle = (e) => {
+    setTitle(e.target.value)
+  }
+
+  const handleDate = (e) => {
+    setDate(e.target.value)
+  }
 
   return (
     <div>
       <form className={styles.appointmentForm}>
         <div>
           <label>Title</label>
-          <input type="text" placeholder="Title" value={title} onChange={()=>{}}/>
+          <input type="text" placeholder="Title" value={title} onChange={handleTitle}/>
         </div>
         <div>
           <label>Booked Date</label>
-          <input type="text" placeholder={getTodayString} value={date} disabled/>
+          <input type="text" placeholder={getTodayString} value={getTodayString()} disabled/>
         </div>
         <div>
-          <label>time appointment data</label>
-          <input type="date"/>
+          <label>Date Appointment</label>
+          <input type="date" value={date} onChange={handleDate}/>
+        </div>
+        <div>
+          <button
+            onClick={handleSubmit}
+            className={styles.btn}
+            >
+              Submit
+          </button>
         </div>
       </form>
     </div>
