522b7e4e1d11b2eb494e0e983d87d566eb4fa5c7
ThaoCV <che.van.thao@jam-tech.jp>
2022-11-11 15:15:50 +0900

    appointment session

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { ContactPicker } from "../contactPicker/ContactPicker";
+import styles from "../appointmentForm/AppointmentForm.module.scss"
 
 export const AppointmentForm = ({
   contacts,
@@ -20,6 +22,21 @@ export const AppointmentForm = ({
   };
 
   return (
-    AppointmentForm
+    <div>
+      <form className={styles.appointmentForm}>
+        <div>
+          <label>Title</label>
+          <input type="text" placeholder="Title" value={title} onChange={()=>{}}/>
+        </div>
+        <div>
+          <label>Booked Date</label>
+          <input type="text" placeholder={getTodayString} value={date} disabled/>
+        </div>
+        <div>
+          <label>time appointment data</label>
+          <input type="date"/>
+        </div>
+      </form>
+    </div>
   );
 };
--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
 
-export const AppointmentsPage = () => {
+export const AppointmentsPage = (props) => {
   /*
   Define state variables for 
   appointment info
   */
+ const [title, setTitle] = useState([])
+ const [date, setDate] = useState("")
+ const [time, setTime] = useState(null)
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,6 +22,18 @@ export const AppointmentsPage = () => {
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm 
+            handleSubmit={handleSubmit}
+            title={title}
+            setTitle={setTitle}
+            date={date}
+            setDate={setDate}
+            time={time}
+            setTime={setTime}
+            contacts={props.contacts}
+            contact={props.contact}
+            setContact={props.setContact}
+        />
       </section>
       <hr />
       <section>
