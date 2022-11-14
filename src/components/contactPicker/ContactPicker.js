32b91eff15bf5685fe482f25b3092e6fef25da9e
Che Van Thao <chevanthao96@gmail.com>
2022-11-14 12:40:06 +0900

    Appointment Finished

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./ContactPicker.module.scss"
 
-export const ContactPicker = () => {
+export const ContactPicker = (props) => {
   return (
-    ContactPicker
+    <div className={styles.ContactPicker}>
+      <h2 className={styles.ContactPickerHeader}>{props.appointment.title}</h2>
+      <div className={styles.ContactPickerDiv}>Appointment {props.appointment.date}</div>
+      <div className={styles.ContactPickerDiv}>Booked Date {props.appointment.bookedDate}</div>
+    </div>
   );
 };
--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -7,12 +7,29 @@ export const AppointmentsPage = (props) => {
   Define state variables for 
   appointment info
   */
- const [title, setTitle] = useState([])
+ const [title, setTitle] = useState("")
  const [date, setDate] = useState("")
  const [time, setTime] = useState(null)
 
+  const getTodayString = () => {
+    const [month, day, year] = new Date()
+      .toLocaleDateString("en-US")
+      .split("/");
+    return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
+  };
+
+  let appointment = {
+    title: title,
+    date: date,
+    bookedDate: getTodayString()
+  }
   const handleSubmit = (e) => {
+    console.log(appointment)
     e.preventDefault();
+    props.addAppointment(appointment)
+    setTitle("")
+    setDate("")
+
     /*
     Add contact info and clear data  
     */
@@ -39,7 +56,11 @@ export const AppointmentsPage = (props) => {
       <hr />
       <section>
         <h2>Appointments</h2>
-        <ContactPicker />
+        {props.appointments.map((appointment, index) => {
+          return (
+            <ContactPicker key={index} appointment={appointment} />
+          )
+        })}
       </section>
     </div>
   );
